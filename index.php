34dc3b11884fcb86d3b819beee483f6cb1e5071e
SadratMetalfire <sebastianderegowski@onet.pl>
2025-05-14 21:21:58 +0200

    Partials and SQL commands

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="main">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Widoczni</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+</div>
---
+++ partials/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    Footer/Wykonał Sebastian Deręgowski
+</footer>
+
+<script src="/assets/js/scripts.js"></script>
+
+</body>
+
+</html>
